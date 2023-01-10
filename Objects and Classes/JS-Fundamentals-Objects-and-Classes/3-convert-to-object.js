87187b346cf08a3e67709b7d141b98adb0c4e5e7
MonicaStoyanova <114282401+MonicaStoyanova@users.noreply.github.com>
2023-01-10 13:51:16 +0200

    Delete Objects and Classes/JS-Fundamentals-Objects-and-Classes directory

--- Objects and Classes/JS-Fundamentals-Objects-and-Classes/3-convert-to-object.js
+++
@@ -1,9 +0,0 @@
-function covert(data) {
-    let info = JSON.parse(data);
-
-    for (const key of Object.keys(info)) {
-        console.log(`${key}: ${info[key]}`);
-    }
-}
-
-covert('{"name": "George", "age": 40, "town": "Sofia"}');
